0b7885b19ad4b0f0128d97fdace109285d9113f5
tarun028 <tarunsrivastavakvs@gmail.com>
2023-06-15 03:59:21 +0530

    First Screen 70%

--- app/src/main/java/com/example/adatask/MainActivity.java
+++ app/src/main/java/com/example/adatask/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.adatask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     ViewPagerAdapter viewPagerAdapter;
+    BottomNavigationView bottomNavigationView;
     FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,55 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tablayout);
         viewPager2 = findViewById(R.id.viewPager);
-        
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        frameLayout = findViewById(R.id.frameLayout);
 
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.VISIBLE);
+                frameLayout.setVisibility(View.GONE);
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                viewPager2.setVisibility(View.GONE);
+                frameLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                    case 1:
+                    case 2:
+                        tabLayout.getTabAt(position).select();
+                }
+                super.onPageSelected(position);
+            }
+        });
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                frameLayout.setVisibility(View.VISIBLE);
+                viewPager2.setVisibility(View.GONE);
+                switch (item.getItemId()){
+                    case R.id.bottomNav:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new Explore()).commit();
+                }
+                return true;
+            }
+        });
     }
 }
