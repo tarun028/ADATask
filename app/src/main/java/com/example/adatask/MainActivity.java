c59e3bfdf119cb2a09c62d755d906a6346eefaf2
tarun028 <tarunsrivastavakvs@gmail.com>
2023-06-15 03:18:23 +0530

    fragments

--- app/src/main/java/com/example/adatask/MainActivity.java
+++ app/src/main/java/com/example/adatask/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.adatask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    ViewPagerAdapter viewPagerAdapter;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager2 = findViewById(R.id.viewPager);
+        
 
     }
 }
--- app/src/main/java/com/example/adatask/Merchant.java
+++ app/src/main/java/com/example/adatask/Merchant.java
@@ -9,14 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class Business extends Fragment {
-
+public class Merchant extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_business, container, false);
+        return inflater.inflate(R.layout.fragment_merchant, container, false);
     }
 }
