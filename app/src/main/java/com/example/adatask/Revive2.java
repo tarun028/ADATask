d1fa492085600b13c35932ca77e89fae028a95bb
tarun028 <tarunsrivastavakvs@gmail.com>
2023-06-16 00:30:30 +0530

    day3

---
+++ app/src/main/java/com/example/adatask/Revive2.java
@@ -0,0 +1,14 @@
+package com.example.adatask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Revive2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revive2);
+    }
+}
